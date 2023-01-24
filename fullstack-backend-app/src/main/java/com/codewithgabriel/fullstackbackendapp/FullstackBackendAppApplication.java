9a376e5dd5e28723674d72be763605a62c79c16e
Joga50 <jose.gabriel.a12@gmail.com>
2023-01-24 15:39:32 -0500

    setting up dev enviorment and installing springboot server

---
+++ fullstack-backend-app/src/main/java/com/codewithgabriel/fullstackbackendapp/FullstackBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.codewithgabriel.fullstackbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendAppApplication.class, args);
+	}
+
+}
---
+++ fullstack-backend-app/src/test/java/com/codewithgabriel/fullstackbackendapp/FullstackBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithgabriel.fullstackbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
