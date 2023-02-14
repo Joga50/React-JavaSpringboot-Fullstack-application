4b9ca06b82852295e0ccd58ea19a60fc26823fc7
Joga50 <jose.gabriel.a12@gmail.com>
2023-02-14 18:30:12 -0500

    adding java as main langauge

--- client/src/layout/NavBar.js
+++ client/src/layout/NavBar.js
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg  navbar-dark bg-secondary bg-body-tertiary">
+      <nav
+        className="navbar navbar-expand-lg  navbar-dark bg-secondary bg-body-tertiary"
+        style={{ height: "300px" }}
+      >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Full Stack Application
+          <Link className="navbar-brand" to="/" style={{ marginLeft: "50px" }}>
+            Demo Full Stack Application
           </Link>
           <button
             className="navbar-toggler"
@@ -20,7 +23,11 @@ export default function NavBar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <Link className="btn btn-outline-light" to="/addUser">
+          <Link
+            className="btn btn-outline-light"
+            to="/addUser"
+            style={{ marginRight: "70px" }}
+          >
             Add User
           </Link>
         </div>
